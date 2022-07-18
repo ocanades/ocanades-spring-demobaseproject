72f2f808c321d350eedb1340c4daaa96fd2663e7
oriolcd <oriol.canades@in2.es>
2022-07-18 02:02:45 +0200

    - Changed pom.xml (new dependencies - validator, mapstruck - and plugins) - New UserException - New UserDto - Changed UserController (added new endpoints) - Changed User (log id  to String UUID) - New UserMapper (mapstruck) - Changed UserRepository (changed Long to String) - Changed UserService and UserServiceImpl (modified Long to UUID) - Changed data.sql (new statement create table if not exists) - Changed CHANGELOG (added new info version)

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# CHANGELOG
+============
+
+v.1.0.0
+=======
+- New User, UserRepository, UserController, and UserService.
+- New basic database data with 4 users.
+- New application configuration file.
+- Added the information of the project.
+
+v.1.1.0
+=======
+# new features
+- New UserException
+- New UserDto (Validators, @JsonProperty, @JsonAlias)
+- New UserMapper (mapstruck)
+# changes from v.1.0.0
+- Changed pom.xml (new dependencies -validator, mapstruck-, new plugins, and version)
+- Changed UserController (added new endpoints, added DtoPattern)
+- Changed User (log id  to String UUID)
+- Changed UserRepository (changed Long to String)
+- Changed UserService and UserServiceImpl (modified Long to UUID)
+- Changed data.sql (new statement create table if not exists)
--- demobaseproject/src/main/java/com/ocanades/demo/services/UserServiceImpl.java
+++ demobaseproject/src/main/java/com/ocanades/demo/services/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package com.ocanades.demo.services;
 
 import com.ocanades.demo.entities.User;
+import com.ocanades.demo.exceptions.UserException;
 import com.ocanades.demo.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -15,13 +17,47 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
-    public List<User> getAllUsers() throws NoSuchElementException {
+    public List<User> getAllUsers() throws UserException {
         List<User> users = userRepository.findAll();
-        if(!users.isEmpty()) {
+        if (!users.isEmpty()) {
             return users;
         } else {
-            throw new NoSuchElementException();
+            throw new UserException("There are no users in the database");
         }
+    }
+
+    @Override
+    public User getUserById(String id) throws UserException {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            return optionalUser.get();
+        } else {
+            throw new UserException("User does not exist");
+        }
+    }
+
+    @Override
+    public void addUser(User user) throws UserException {
+        if (checkIfUserExists(user)) {
+            throw new UserException("User cannot be created");
+        } else {
+            User newUser = User.builder()
+                    .id(UUID.randomUUID().toString())
+                    .firstName(user.getFirstName())
+                    .lastName(user.getLastName())
+                    .email(user.getEmail())
+                    .build();
+            saveUser(newUser);
+        }
+    }
+
+    private boolean checkIfUserExists(User newUser) {
+        Optional<User> optionalUser = userRepository.findByEmail(newUser.getEmail());
+        return optionalUser.isPresent();
+    }
+
+    private void saveUser(User user) {
+        userRepository.save(user);
     }
 
 }
