b39c4f3666b8c230abfa0cb42449e97267b32105
oriolcd <oriol.canades@in2.es>
2022-07-19 17:28:17 +0200

    - Changed pom.xml (new Spring Security dependencies) - Changed DemoApplication.java (new endpoint for monitoring DemoApplication is running) - Changed configuration yml file (new configuration for Spring Security) - New custom WebSecurityConfiguration file (work in progress...)

--- demobaseproject/src/main/java/com/ocanades/demo/DemoApplication.java
+++ demobaseproject/src/main/java/com/ocanades/demo/DemoApplication.java
@@ -2,9 +2,17 @@ package com.ocanades.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+	@RequestMapping("/")
+	public String monitor() {
+		return "DemoApplication API is running...";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
